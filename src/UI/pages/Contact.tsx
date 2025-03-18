b5a3eec06e757641e12da1d6f26796cdb73de8b7
Aaquib Tanweer <aaquib.tanweer.111@gmail.com>
2025-03-18 16:44:58 +0530

    Update Contact.tsx

--- src/UI/pages/Contact.tsx
+++ src/UI/pages/Contact.tsx
@@ -1,6 +1,6 @@
-import { companyDataConstants } from '@/constants/data/companyData.constant'
-import { Input } from '@/UI/shadcn/ui/input'
-import { Button } from '@/UI/shadcn/ui/button'
+import { companyDataConstants } from '@/constants/data/companyData.constant';
+import { Input } from '@/UI/shadcn/ui/input';
+import { Button } from '@/UI/shadcn/ui/button';
 
 export const Contact = () => {
   return (
@@ -9,11 +9,11 @@ export const Contact = () => {
         Let's <span className="underline">create</span> something together.
       </h1>
       <div className="flex gap-2">
-        <Input placeholder="info@envisionedgetech.com" className="border-blue-500/20" />
-        <a href={`mailto:${companyDataConstants.email}`}>
+        <Input placeholder={companyDataConstants.emails[0] || "info@envisionedgetech.com"} className="border-blue-500/20" />
+        <a href={`mailto:${companyDataConstants.emails[0]}`}>
           <Button className="bg-blue-500 hover:bg-blue-600 text-white">Send Email</Button>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
