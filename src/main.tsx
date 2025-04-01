49a6cbb57d538f99345e8f2fe0506e3e4dcc294e
Aaquib Tanweer <aaquib.tanweer.111@gmail.com>
2025-04-02 02:16:18 +0530

    prod fix #3

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,18 @@
-import { StrictMode } from 'react'
+import { RouterProvider } from '@tanstack/react-router'
+import { router } from './routes'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import './index.css'
-
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import './assets/global.css'
 import { ThemeProvider } from './components/theme-provider'
 
-// Create a new router instance
-const router = createRouter({ routeTree })
-
-// Register the router instance for type safety
+// Register your router before using it
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
   }
 }
 
-// Render the app
-const rootElement = document.getElementById('root')!
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <StrictMode>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </StrictMode>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <RouterProvider router={router} />
+  </ThemeProvider>
+)
