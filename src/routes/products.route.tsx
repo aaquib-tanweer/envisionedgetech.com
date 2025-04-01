237e9791490f0cc8d0aa3bdd5824a058571bce9b
Aaquib Tanweer <aaquib.tanweer.111@gmail.com>
2025-04-02 02:17:45 +0530

    final prod fix

--- src/routes/products.route.tsx
+++ src/routes/products.route.tsx
@@ -1,13 +1,7 @@
 import { Layout } from '@/UI/pages/Layout'
-import { ProductsPage } from '@/UI/pages/Products'
+import { Products } from '@/UI/pages/Products'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/products')({
-  component: () => (
-    <>
-      <Layout>
-        <ProductsPage />
-      </Layout>
-    </>
-  ),
+  component: Products
 })
