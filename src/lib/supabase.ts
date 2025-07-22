65d3a953a044bfa4c9af456f5b66df67e0bbc7e0
Aaquib Tanweer <aaquib.tanweer.111@gmail.com>
2025-07-23 00:01:23 +0530

    added session timeout on refresh and new tab for admin backend database

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://bfzpkgkuronfiwtbratp.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmenBrZ2t1cm9uZml3dGJyYXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1MDY5MjgsImV4cCI6MjA1ODA4MjkyOH0.OSdHo712YbH5qQnev7ZikHFu5J7oLPE-Vcj_rE_oH4k'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: sessionStorage,
+    persistSession: false,
+    autoRefreshToken: false
+  }
+}) 
